00bfde98c30677c9f6e6d7cc6eb9eee1a0bb2cdc
AngelZapata582 <angelzapata582@gmail.com>
2022-07-19 22:41:02 -0500

    agregado funcionalidad del modulo de admin para obtener datos con api

--- src/App.js
+++ src/App.js
@@ -2,28 +2,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Reproductor from './components/componentes/player/audio.js';
-import FilterableProductTable from './components/componentes/admin/admin.js';
+import FilterTableMusic from './components/componentes/admin/admin.js';
 const music = [
   {nombre:"track1/mp3"},
   {nombre:"track2/opus"},
   {nombre:"track3/mp3"}
 ]
 
-const PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
 
 function App() {
   return (
     <div className="App">
       <Reproductor music={music}/>
-      
-      <FilterableProductTable products={PRODUCTS} />
+      <FilterTableMusic/>
     </div>
   );
 }
