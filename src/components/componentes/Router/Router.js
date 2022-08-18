ccdc3c170366227b4f19646bae20a385f394dc6c
AngelZapata582 <angelzapata582@gmail.com>
2022-08-18 17:38:23 -0500

    agregado de la ruta para error 404

--- src/components/componentes/Router/Router.js
+++ src/components/componentes/Router/Router.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FilterTableMusic from "../admin/admin.js";
 import Login from "../../js/login.js";
+import Registro from "../../js/registro.js";
+import Inicio from "../inicio/inicio.js";
+import Perfil from "../perfil/perfil.js";
+import NotFound from "../notfound/NotFound.js"
+
 export default class RouterPaths extends React.Component{
     render(){
         return (
             <Router forceRefresh={true}>
                 <Routes>
+                    <Route exact path="/perfil/:id" element={<Perfil/>}/>
+                    <Route exact path="/" element={<Inicio/>}/>
                     <Route exact path="/admin" element={<FilterTableMusic/>}/>
-                    <Route render={() => <h1>404: Page Not Found</h1>}/>
+                    <Route path="*" element={<NotFound/>}/>
                     <Route exact path="/login" element={<Login/>}/>
+                    <Route exact path="/registro" element={<Registro/>}/>
                 </Routes>
             </Router>
         )
---
+++ src/components/componentes/notfound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PageNotFound extends React.Component {
+    render() {
+        return <div class="container">
+            <div class="my-5">
+            <h1 class="text-primary">Error 404!!</h1>
+            <h1 class="text-primary">Pagina no encontrada</h1>
+            </div>
+        </div>
+    }
+}
+
+export default PageNotFound
